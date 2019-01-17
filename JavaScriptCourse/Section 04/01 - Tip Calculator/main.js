64eb6c17258e2b1a31dd2bbf2783b141da2cdd0d
William M <wmooney1984@gmail.com>
2019-01-17 06:49:39 -0500

    JS Course: Section 4

--- JavaScriptCourse/Section 04/01 - Tip Calculator/main.js
+++ JavaScriptCourse/Section 04/01 - Tip Calculator/main.js
@@ -1,1 +1,12 @@
-alert("Working");
+
+
+
+function calculateTip (amount, tip) {
+  let percent = tip / 100;
+  let tipAmount = amount * percent;
+  let totalAmount = amount + tipAmount;
+
+  console.log(`Tip Percentage: ${percent}\nTip Amount: ${tipAmount}\nTotal Amount: ${totalAmount}`);
+}
+
+calculateTip(100, 15);
--- JavaScriptCourse/Section 04/10 - ToDo List/main.js
+++ JavaScriptCourse/Section 04/10 - ToDo List/main.js
@@ -1,1 +1,40 @@
-alert("Working");
+
+let toDOList = [];
+
+// function addItem(arr, item) {
+//  arr.push(item);
+// }
+
+// addItem(toDOList, "Walk the dog");
+// addItem(toDOList, "Make Coffee");
+
+// console.log(toDOList);
+
+function addItem(arr) {
+  let answer = prompt("New task to add: ");
+  let correct = answer.toLowerCase().trim();
+
+  if(checkDuplicate(arr, correct)) {
+    arr.push(correct);
+  } else {
+    alert("Item already exists.");
+  }
+}
+
+function checkDuplicate (arr, item) {
+  let exists = false;
+  let index = arr.indexOf(item);
+  if (index != -1) {
+    exists = false;
+  } else {
+    exists = true;
+  }
+
+  return exists;
+}
+
+addItem(toDOList);
+addItem(toDOList);
+addItem(toDOList);
+
+console.log(toDOList);
